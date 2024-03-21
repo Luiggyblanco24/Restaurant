6c3607808d9843b34a2f4558c8c5f75975c59cc6
Luiggyblanco24 <24luiggy@gmail.com>
2024-03-20 19:10:43 -0500

    areglo error del menu

--- restaurant-main/restaurant-main/restaurantApp/src/main/java/com/proyectoali/restaurantapp/RestaurantApp.java
+++ restaurant-main/restaurant-main/restaurantApp/src/main/java/com/proyectoali/restaurantapp/RestaurantApp.java
@@ -1,9 +1,10 @@
 package com.proyectoali.restaurantapp;
-import java.util.ArrayList;
 
-import com.proyectoali.restaurantapp.foods;;
+import com.proyectoali.restaurantapp.foods.Dish;
 import com.proyectoali.restaurantapp.foods.Menu;
 import com.proyectoali.restaurantapp.people.User;
+import com.proyectoali.restaurantapp.values.DishTypes;
+import com.proyectoali.restaurantapp.values.DishVariation;
 import com.proyectoali.restaurantapp.values.UserTypes;
 
 
@@ -15,11 +16,16 @@ public class RestaurantApp {
         
         Menu menu = new Menu();
         
-        Dish dish1 = new Dish();
-        menu.addDish(dish1);
+        Dish dish1 = new Dish(DishTypes.BREAKFAST, "huevos fritos", 20, 2400, DishVariation.OPTION1);
+        Dish dish2 = new Dish(DishTypes.LUNCH, "Arroz con carne ", 15, 2400, DishVariation.OPTION2); 
+        Dish dish3 = new Dish (DishTypes.DINER, "Calentado", 20, 2400, DishVariation.OPTION3);
         
+        menu.addDish(dish1);
+        menu.addDish(dish2);
+        menu.addDish(dish3);
         
         menu.showMenu();
+        System.out.println(menu);
         
         
-    }
+    }}
--- restaurant-main/restaurant-main/restaurantApp/src/main/java/com/proyectoali/restaurantapp/foods/Dish.java
+++ restaurant-main/restaurant-main/restaurantApp/src/main/java/com/proyectoali/restaurantapp/foods/Dish.java
@@ -1,25 +1,24 @@
-
 package com.proyectoali.restaurantapp.foods;
 
 import com.proyectoali.restaurantapp.values.DishTypes;
 import com.proyectoali.restaurantapp.values.DishVariation;
 
 
- class Dish {
+ public class Dish {
     DishTypes type;
     String name;
     int stock;
     int price;
     DishVariation variation;
-    String ingidents;
+  
 
-    public Dish(DishTypes type, String name, int stock, int price, DishVariation variation, String ingidents) {
+    public Dish(DishTypes type, String name, int stock, int price, DishVariation variation ) {
         this.type = type;
         this.name = name;
         this.stock = stock;
         this.price = price;
         this.variation = variation;
-        this.ingidents = ingidents;
+        
     }
     
 }
